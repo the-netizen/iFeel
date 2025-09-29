db11b02ff4fdedc77134d9e1716a093b7bfe988a
the-netizen <khanaima020815@gmail.com>
2025-09-29 16:57:30 +0300

    Initial Commit

---
+++ iFeel/iFeelApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFeelApp.swift
+//  iFeel
+//
+//  Created by Naima Khan on 29/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct iFeelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
